3d88d71aa358d5559081433a3ae68e72ef31d2af
hari <sjhariharan@gmail.com>
2025-09-03 18:01:26 +0530

    done

--- src/main/java/com/jsipder/SpringDemo/App.java
+++ src/main/java/com/jsipder/SpringDemo/App.java
@@ -1,10 +1,22 @@
 package com.jsipder.SpringDemo;
 
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import com.jsipder.SpringDemo.dto.Student;
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        ApplicationContext context=new ClassPathXmlApplicationContext("META-INF/beans.xml");//by using this it created IOC container
+        System.out.println("IOC container is created");
+        context.getBean(Student.class);
+        System.out.println(context.getBean(Student.class));
+        //context=new FileSystemXmlApplicationContext("xml-file");
+ //        context =new AnnotationConfigApplicationContext("java-file");
     }
 }
--- src/main/java/com/jsipder/SpringDemo/dto/Spring.java
+++
@@ -1,8 +0,0 @@
-package com.jsipder.SpringDemo.dto;
-
-public class Spring {
-	public static void main(String[] args) {
-		Spring sp=new Spring();
-	}
-
-}
